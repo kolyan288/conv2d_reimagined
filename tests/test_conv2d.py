80ed776421325e51a9f1007d15ca79865f3971df
Kolyan288 <smoljanov13@mail.ru>
2025-10-21 18:13:51 +0000

    Update img2col layer test

--- tests/test_conv2d.py
+++ tests/test_conv2d.py
@@ -5,6 +5,123 @@ from torch.nn.parameter import Parameter
 import math
 
 
+class Img2ColConvFunction(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, X, weight, bias, stride, padding, padding_mode):
+        ctx.save_for_backward(X, weight, bias)
+        ctx.stride = stride
+        ctx.padding = padding
+        ctx.padding_mode = padding_mode
+
+        N, C_in, H, W = X.shape
+        C_out, _, kernel_h, kernel_w = weight.shape
+
+        H_out = (H + 2 * padding - kernel_h) // stride + 1
+        W_out = (W + 2 * padding - kernel_w) // stride + 1
+
+        if padding > 0:
+            if padding_mode == "zeros":
+                X_padded = F.pad(X, (padding, padding, padding, padding))
+            elif padding_mode == "reflect":
+                X_padded = F.pad(
+                    X, (padding, padding, padding, padding), mode="reflect"
+                )
+            elif padding_mode == "replicate":
+                X_padded = F.pad(
+                    X, (padding, padding, padding, padding), mode="replicate"
+                )
+            else:
+                raise NotImplementedError(
+                    f"Padding mode {padding_mode} not implemented"
+                )
+        else:
+            X_padded = X
+
+        patches = X_padded.unfold(2, kernel_h, stride).unfold(3, kernel_w, stride)
+        patches_reshaped = patches.permute(0, 2, 3, 1, 4, 5).contiguous()
+        patches_reshaped = patches_reshaped.view(N * H_out * W_out, -1)
+
+        weight_reshaped = weight.view(C_out, -1)
+        output = torch.mm(patches_reshaped, weight_reshaped.t())
+
+        if bias is not None:
+            output += bias.unsqueeze(0)
+
+        output = output.view(N, H_out, W_out, C_out).permute(0, 3, 1, 2)
+        return output
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        X, weight, bias = ctx.saved_tensors
+        stride = ctx.stride
+        padding = ctx.padding
+        padding_mode = ctx.padding_mode
+
+        N, C_in, H, W = X.shape
+        C_out, _, kernel_h, kernel_w = weight.shape
+        H_out = (H + 2 * padding - kernel_h) // stride + 1
+        W_out = (W + 2 * padding - kernel_w) // stride + 1
+
+        grad_output_reshaped = (
+            grad_output.permute(0, 2, 3, 1).contiguous().view(N * H_out * W_out, C_out)
+        )
+
+        if bias is not None:
+            grad_bias = grad_output.sum(dim=[0, 2, 3])
+        else:
+            grad_bias = None
+
+        if padding > 0:
+            if padding_mode == "zeros":
+                X_padded = F.pad(X, (padding, padding, padding, padding))
+            elif padding_mode == "reflect":
+                X_padded = F.pad(
+                    X, (padding, padding, padding, padding), mode="reflect"
+                )
+            elif padding_mode == "replicate":
+                X_padded = F.pad(
+                    X, (padding, padding, padding, padding), mode="replicate"
+                )
+        else:
+            X_padded = X
+
+        patches = X_padded.unfold(2, kernel_h, stride).unfold(3, kernel_w, stride)
+        patches_reshaped = (
+            patches.permute(0, 2, 3, 1, 4, 5).contiguous().view(N * H_out * W_out, -1)
+        )
+
+        grad_weight_reshaped = torch.mm(grad_output_reshaped.t(), patches_reshaped)
+        grad_weight = grad_weight_reshaped.view(C_out, C_in, kernel_h, kernel_w)
+
+        weight_reshaped = weight.view(C_out, -1)
+        grad_patches = torch.mm(grad_output_reshaped, weight_reshaped)
+        grad_patches_reshaped = grad_patches.view(
+            N, H_out, W_out, C_in, kernel_h, kernel_w
+        )
+        grad_patches_permuted = grad_patches_reshaped.permute(
+            0, 3, 1, 2, 4, 5
+        ).contiguous()
+        grad_patches_flat = grad_patches_permuted.view(
+            N, C_in * kernel_h * kernel_w, H_out * W_out
+        )
+
+        fold = torch.nn.Fold(
+            output_size=(X_padded.shape[2], X_padded.shape[3]),
+            kernel_size=(kernel_h, kernel_w),
+            stride=stride,
+            padding=0,
+            dilation=1,
+        )
+        grad_input_padded = fold(grad_patches_flat)
+
+        if padding > 0:
+            grad_input = grad_input_padded[:, :, padding:-padding, padding:-padding]
+        else:
+            grad_input = grad_input_padded
+
+        return grad_input, grad_weight, grad_bias, None, None, None
+
+
 class Conv2dImg2Col(nn.Module):
     def __init__(
         self,
@@ -51,48 +168,9 @@ class Conv2dImg2Col(nn.Module):
             nn.init.uniform_(self.bias, -bound, bound)
 
     def forward(self, x):
-        return self._img2col_conv(x, self.weight, self.bias, self.stride, self.padding)
-
-    def _img2col_conv(self, X, weight, bias=None, stride=1, padding=0):
-        N, C_in, H, width = X.shape
-        C_out, _, kernel_h, kernel_w = weight.shape
-
-        H_out = (H + 2 * padding - kernel_h) // stride + 1
-        W_out = (width + 2 * padding - kernel_w) // stride + 1
-
-        if padding > 0:
-            if self.padding_mode == "zeros":
-                X_padded = F.pad(X, (padding, padding, padding, padding))
-            elif self.padding_mode == "reflect":
-                X_padded = F.pad(
-                    X, (padding, padding, padding, padding), mode="reflect"
-                )
-            elif self.padding_mode == "replicate":
-                X_padded = F.pad(
-                    X, (padding, padding, padding, padding), mode="replicate"
-                )
-            else:
-                raise NotImplementedError(
-                    f"Padding mode {self.padding_mode} not implemented"
-                )
-        else:
-            X_padded = X
-
-        patches = X_padded.unfold(2, kernel_h, stride).unfold(3, kernel_w, stride)
-
-        patches_reshaped = patches.permute(0, 2, 3, 1, 4, 5).contiguous()
-        patches_reshaped = patches_reshaped.view(N * H_out * W_out, -1)
-
-        weight_reshaped = weight.view(C_out, -1)
-
-        output = torch.mm(patches_reshaped, weight_reshaped.t())
-
-        if bias is not None:
-            output += bias.unsqueeze(0)
-
-        output = output.view(N, H_out, W_out, C_out).permute(0, 3, 1, 2)
-
-        return output
+        return Img2ColConvFunction.apply(
+            x, self.weight, self.bias, self.stride, self.padding, self.padding_mode
+        )
 
     def extra_repr(self):
         s = (
